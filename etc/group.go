046350f1ffe5e985037a97b8a357f682cf891567
Nick Imkamp <imkampn@gmail.com>
2019-06-08 14:14:00 -0700

    Parse /etc/group
    Create function to parse out /etc/group file
    
    Store values in struct and store struct in an array.

---
+++ etc/group.go
@@ -0,0 +1,42 @@
+package etc
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strings"
+)
+
+func ParseGroup(r io.Reader) ([]GroupEntry, error) {
+	var entries []GroupEntry
+
+	lr := bufio.NewReader(r)
+
+	for {
+		line, _, err := lr.ReadLine()
+		if err != nil {
+			break
+		}
+
+		splitLine := strings.Split(string(line), ":")
+		if len(splitLine) != 4 {
+			return nil, fmt.Errorf("etcgroup.Parse: expected 3 semi colons on parsed line but got %d", len(splitLine))
+		}
+
+		e := GroupEntry{
+			Name:    splitLine[0],
+			GroupID: splitLine[1],
+			Members: splitLine[2],
+		}
+
+		entries = append(entries, e)
+	}
+
+	return entries, nil
+}
+
+type GroupEntry struct {
+	Name    string
+	GroupID string
+	Members string
+}
