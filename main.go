482e3aeb9e8817ec863e54c2f67b021e03c643ff
Nick Imkamp <imkampn@gmail.com>
2019-06-16 17:49:58 -0700

    Create api endpoints and test data
    Create api endpoints for /users, /groups, users/uid, and /groups/gid.
    Create functions to return an array of entries

--- main.go
+++ main.go
@@ -2,28 +2,152 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
 
-	"github.com/pass-as-a-service/etc"
+	"github.com/go-chi/chi"
+	"github.com/nimkamp/pass-as-a-service/etc"
 )
 
 func main() {
-	log.Println("\nFrom byte array")
-	for _, entry := range fromByteArrayExample() {
-		log.Printf("%v", entry)
+	var pTestBytes = []byte(`nick:x:1:1::/Users/home/nick:/bin/bash
+george:x:3:2::/Users/home/george:/bin/sh
+docker:x:321:1002::/Users/home/george:/bin/sh
+`)
+	var gTestBytes = []byte(`nick:x:1:bob
+george:x:3:321
+docker:x:1002:dwoodlins
+`)
+
+	pEntries, err := etc.ParsePasswd(bytes.NewReader(pTestBytes))
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	log.Println("\nFrom file")
-	for _, entry := range fileExample() {
-		log.Printf("%v", entry)
+	gEntries, err := etc.ParseGroup(bytes.NewReader(gTestBytes))
+	if err != nil {
+		log.Fatal(err)
 	}
 
+	pEntriesJson, err := json.Marshal(pEntries)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	gEntriesJson, err := json.Marshal(gEntries)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// log.Println("\nFrom byte array")
+	// for _, entry := range fromByteArrayExample() {
+	// 	log.Printf("%v", entry)
+	// }
+
+	// log.Println("\nFrom file")
+	// for _, entry := range fileExample() {
+	// 	log.Printf("%v", entry)
+	// }
+
+	// log.Println("\n Group From byte array")
+	// for _, entry := range fromByteArrayExampleGroup() {
+	// 	log.Printf("%v", entry)
+	// }
+
+	// log.Println("\n Group From file")
+	// for _, entry := range fileExampleGroup() {
+	// 	log.Printf("%v", entry)
+	// }
+
+	// log.Println("\n Return a user based off uid (GET /users/<uid>) byte array")
+	// pEntry, err := etc.FindPasswdEntryByID("321", fromByteArrayExample())
+	// if err != nil {
+	// 	log.Println(err.Error())
+	// }
+	// log.Printf("%v\n", pEntry)
+
+	// log.Println("\n Return all groups for a given user (GET /groups/gid)")
+	// gEntry, err := etc.FindGroupEntryByID("3", fromByteArrayExampleGroup())
+	// if err != nil {
+	// 	log.Println(err.Error())
+	// }
+	// log.Printf("%v\n", gEntry)
+
+	// log.Println("\n Return all groups for a given user (GET /users/<uid>/groups)")
+	// gEntry, err := etc.FindGroupEntryByUserID("321", fromByteArrayExample(), fromByteArrayExampleGroup())
+	// if err != nil {
+	// 	log.Println(err.Error())
+	// }
+	// log.Printf("%v\n", gEntry)
+
+	// log.Println("\n Users with group id=1002")
+	// pEntry, err := etc.GetPasswdByQuery("docker", "", "1002", "", "", "", fromByteArrayExample())
+	// if err != nil {
+	// 	log.Println(err.Error())
+	// }
+	// log.Printf("%v\n", pEntry)
+
+	// log.Println("\n Groups with group id=1002")
+	// pEntry, err := etc.GetGroupByQuery("george", "", "", fromByteArrayExampleGroup())
+	// if err != nil {
+	// 	log.Println(err.Error())
+	// }
+	// log.Printf("%v\n", pEntry)
+
+	r := chi.NewRouter()
+	r.Get("/users", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(pEntriesJson))
+	})
+
+	r.Get("/users/{uid}", func(w http.ResponseWriter, r *http.Request) {
+		uid := chi.URLParam(r, "uid")
+		entry, err := etc.FindPasswdEntryByID(uid, pEntries)
+		if err != nil {
+			http.Error(w, "User ID does not exist", 404)
+			return
+		}
+
+		jsonEntry, err := json.Marshal(&entry)
+		if err != nil {
+			// http.Error(w, err.Error(), err.Status())
+			return
+
+		}
+		w.Write(jsonEntry)
+		return
+	})
+
+	r.Get("/groups", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(gEntriesJson))
+	})
+
+	r.Get("/groups/{gid}", func(w http.ResponseWriter, r *http.Request) {
+		gid := chi.URLParam(r, "gid")
+		entry, err := etc.FindGroupEntryByID(gid, gEntries)
+		if err != nil {
+			http.Error(w, "Group ID does not exist", 404)
+			return
+		}
+
+		jsonEntry, err := json.Marshal(&entry)
+		if err != nil {
+			// http.Error(w, err.Error(), err.Status())
+			return
+
+		}
+		w.Write(jsonEntry)
+		return
+	})
+
+	http.ListenAndServe(":3000", r)
 }
 
 func fromByteArrayExample() []etc.PasswdEntry {
-	var testBytes = []byte(`nick:x:123:1::/Users/home/nick:/bin/bash
-george:x:321:2::/Users/home/george:/bin/sh
+	var testBytes = []byte(`nick:x:1:1::/Users/home/nick:/bin/bash
+george:x:3:2::/Users/home/george:/bin/sh
+docker:x:321:1002::/Users/home/george:/bin/sh
 `)
 
 	etcPasswdEntries, err := etc.ParsePasswd(bytes.NewReader(testBytes))
@@ -45,4 +169,32 @@ func fileExample() []etc.PasswdEntry {
 		log.Fatal(err)
 	}
 	return etcPasswdEntries
+}
+
+func fromByteArrayExampleGroup() []etc.GroupEntry {
+	var testBytes = []byte(`nick:x:1:bob
+george:x:3:321
+docker:x:1002:dwoodlins
+`)
+
+	etcGroupEntries, err := etc.ParseGroup(bytes.NewReader(testBytes))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return etcGroupEntries
+}
+
+func fileExampleGroup() []etc.GroupEntry {
+	b, err := ioutil.ReadFile("/home/nimkamp/go/src/github.com/pass-as-a-service/fakeetcgroup")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	etcGroupEntries, err := etc.ParseGroup(bytes.NewReader(b))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return etcGroupEntries
 }
